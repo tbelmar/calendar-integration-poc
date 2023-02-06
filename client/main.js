ea0f04b6e971c0be34ba8dce06202ecbf1b6bacb
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2023-02-06 17:57:02 +0100

    Added Google authentication methods

--- client/main.js
+++ client/main.js
@@ -1,9 +1,8 @@
-import { Meteor } from 'meteor/meteor';
-import App from '../imports/ui/App.svelte';
-
+import { Meteor } from "meteor/meteor";
+import App from "../imports/ui/App.svelte";
 
 Meteor.startup(() => {
   new App({
-    target: document.getElementById('app')
+    target: document.getElementById("app"),
   });
 });
--- server/main.js
+++ server/main.js
@@ -1,35 +1,70 @@
-import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { Meteor } from "meteor/meteor";
+import { authenticate } from "@google-cloud/local-auth";
+import { google } from "googleapis";
+import * as path from "path";
 
-async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/calendar"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.env.PWD, "token.json");
+const CREDENTIALS_PATH = path.join(process.env.PWD, "credentials.json");
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
 }
 
-Meteor.publish('links.all', function publishLinksAll() {
-  return LinksCollection.find();
-})
+/**
+ * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  console.log(client);
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
 
 Meteor.startup(async () => {
-  // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
-    await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://svelte-tutorial.meteor.com/',
-    });
-
-    await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
-    });
-
-    await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
-    });
-
-    await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
-    });
-  }
+  const auth = await authorize();
 });
